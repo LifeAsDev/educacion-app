aaa23aa2860d70c11c0723b190ec09ac2236a59f
LifeAsDev <angelocraft551@gmail.com>
2024-05-03 09:24:14 -0400

    curso edit endpoint and css

--- src/app/api/curso/[id]/route.ts
+++ src/app/api/curso/[id]/route.ts
@@ -16,3 +16,32 @@ export async function DELETE(req: Request, { params }: any) {
     );
   else return NextResponse.json({ error: "Curso not found" }, { status: 404 });
 }
+
+export async function PATCH(req: Request, { params }: any) {
+  try {
+    const { id } = params;
+    const { name } = await req.json();
+
+    await connectMongoDB();
+
+    // Buscar el curso por su ID
+    const curso = await Curso.findByIdAndUpdate(id, { name }, { new: true });
+
+    if (curso) {
+      return NextResponse.json(
+        { message: "Curso actualizado exitosamente", curso },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Curso no encontrado" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error al actualizar el curso", details: error },
+      { status: 500 }
+    );
+  }
+}
