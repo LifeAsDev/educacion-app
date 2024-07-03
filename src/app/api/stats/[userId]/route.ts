801376d246a9bf927db105265bb2017716a68c7e
LifeAsDev <angelosarmiento5d@gmail.com>
2024-07-02 21:22:55 -0400

    check all users score initial

--- src/app/api/stats/[userId]/route.ts
+++ src/app/api/stats/[userId]/route.ts
@@ -6,7 +6,6 @@ import mongoose, { Types } from "mongoose";
 import EvaluationOnCourse from "@/schemas/evaluationOnCourse";
 import EvaluationTest from "@/schemas/evaluationTest";
 import EvaluationAssign from "@/schemas/evaluationAssign";
-import EvaluationOnCourseModel from "@/models/evaluationOnCourse";
 
 export async function GET(req: Request, { params }: any) {
   const { searchParams } = new URL(req.url);
@@ -88,6 +87,7 @@ export async function GET(req: Request, { params }: any) {
       percentage,
     });
   }
+
   if (findEvaluationOnCourse) {
     return NextResponse.json(
       {
@@ -112,4 +112,103 @@ export async function GET(req: Request, { params }: any) {
     },
     { status: 404 }
   );
+}
+
+interface Answer {
+  questionId: Types.ObjectId;
+  answer: string;
+}
+
+interface Question {
+  _id: Types.ObjectId;
+  type: string;
+  puntos?: number;
+  openAnswers?: string[];
+}
+
+interface Evaluation {
+  name: string;
+  questionArr: Question[];
+}
+
+interface EvaluationAssign {
+  evaluationId: Evaluation;
+}
+
+interface EvaluationOnCourseModel {
+  _id: Types.ObjectId;
+  evaluationAssignId: EvaluationAssign;
+  answers: Answer[];
+}
+
+interface EvaluationResult {
+  name: string;
+  _id: Types.ObjectId;
+  answersCorrect: number;
+  answersCount: number;
+  percentage: number;
+}
+
+export async function getEvaluationsOnCourse(
+  userId: string,
+  evaluationsOnCourse: EvaluationOnCourseModel[]
+): Promise<{
+  evaluationList: EvaluationResult[];
+  mainPercentage: number;
+  message: string;
+}> {
+  const evaluationList: EvaluationResult[] = [];
+
+  for (const evaluationOnCourse of evaluationsOnCourse) {
+    const progress: number[] = [];
+    let totalPoints = 0;
+
+    for (const question of evaluationOnCourse.evaluationAssignId.evaluationId
+      .questionArr) {
+      const points =
+        !question.puntos || question.puntos === 0 ? 1 : question.puntos;
+      totalPoints += points;
+
+      const answer = evaluationOnCourse.answers.find(
+        (answer) => answer.questionId.toString() === question._id.toString()
+      );
+
+      if (question.type === "multiple") {
+        progress.push(answer && answer.answer === "a" ? points : 0);
+      } else if (question.type === "open") {
+        progress.push(
+          answer && question.openAnswers?.includes(answer.answer) ? points : 0
+        );
+      }
+    }
+
+    const percentage =
+      Math.round(
+        (progress.reduce((acc, val) => acc + val, 0) / totalPoints) * 100
+      ) || 0;
+
+    evaluationList.push({
+      name: evaluationOnCourse.evaluationAssignId!.evaluationId.name,
+      _id: evaluationOnCourse._id,
+      answersCorrect: progress.reduce(
+        (acc, val) => (val > 0 ? acc + 1 : acc),
+        0
+      ),
+      answersCount: progress.length,
+      percentage,
+    });
+  }
+
+  const mainPercentage = Math.round(
+    evaluationList.reduce((acc, curr) => acc + curr.percentage, 0) /
+      evaluationList.length
+  );
+
+  return {
+    evaluationList,
+    mainPercentage,
+    message: evaluationsOnCourse.length
+      ? "Success"
+      : "Not found evaluationsOnCourse",
+  };
 }
