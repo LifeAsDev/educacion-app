fd6e7cdedb97f96f5d330dc972eb9f05050cb26a
LifeAsDev <angelosarmiento5d@gmail.com>
2024-08-08 18:24:39 -0400

    edit evaluation Test

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,5 +1,5 @@
-import Create from "@/components/create/create";
+import CreateV2 from "@/components/create/createV2/createV2";
 
 export default function Page({ params }: { params: any }) {
-  return <Create id={params.id} />;
+  return <CreateV2 id={params.id} />;
 }
