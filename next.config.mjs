e7f6ffae81e56f7fd6bc19bc1198da6a7048a349
LifeAsDev <angelosarmiento5d@gmail.com>
2024-09-27 10:45:46 -0400

    nextconfig minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@
  */
 const nextConfig = {
   images: {
-    domains: [process.env.NEXT_PUBLIC_DOMAIN_CONFIG_BASE_URL || ""],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_DOMAIN_CONFIG_BASE_URL || "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
