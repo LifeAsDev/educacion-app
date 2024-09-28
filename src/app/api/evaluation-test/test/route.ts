80aa0fe015bccb26945fa78adfce5eca4baf9750
LifeAsDev <angelosarmiento5d@gmail.com>
2024-09-28 13:28:46 -0400

    evaluationTest POST endpoint test

---
+++ src/app/api/evaluation-test/test/route.ts
@@ -0,0 +1,28 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import EvaluationTest from "@/schemas/evaluationTest";
+import Question from "@/models/question";
+import { uploadFile } from "@/lib/functionToFiles";
+import { getFileTypeFromBuffer } from "@/lib/functionToFiles";
+import mongoose, { Types } from "mongoose";
+import Asignatura from "@/schemas/asignatura";
+import User from "@/schemas/user";
+import { FilePDF } from "@/models/evaluationTest";
+
+export async function POST(req: Request) {
+  console.log("check0");
+
+  const data = await req.formData();
+  console.log("check1", data);
+
+  return NextResponse.json(
+    {
+      success: false,
+      error: "error retrieving evaluation tests",
+    },
+    { status: 400 }
+  );
+}
+
+export const dynamic = "force-dynamic";
